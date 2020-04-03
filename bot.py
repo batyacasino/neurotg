6f1858dac5735ec97e79aac32ad83b842168b0fa
batyacasino <batyacasino@gmail.com>
2020-04-03 13:06:44 +0300

    huli go

--- bot.py
+++ bot.py
@@ -1,20 +1,14 @@
 import config
 import telebot
-#from telebot import apihelper
 import time
 
 
 bot = telebot.TeleBot(config.token)
-#apihelper.proxy = {'https':'socks5://69.163.160.115:12803'}
-
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
 
 
-while True:
-	try:
-		bot.infinity_polling()
-	except:
-		time.sleep(2)
+if __name__ == '__main__':
+	bot.infinity_polling()
